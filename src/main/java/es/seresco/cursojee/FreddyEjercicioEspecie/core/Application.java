1cc9d1e7b2854c293d503a840b0ba134b9099f04
Freddyfz <freddyfz2312@gmail.com>
2022-04-25 12:05:53 +0200

    FIn clase 25/4

---
+++ src/main/java/es/seresco/cursojee/FreddyEjercicioEspecie/core/Application.java
@@ -0,0 +1,5 @@
+package es.seresco.cursojee.FreddyEjercicioEspecie.core;
+
+public class Application {
+
+}
--- src/test/java/es/seresco/cursojee/FreddyEjercicioEspecie/service/impl/EspecieServiceTestCase.java
+++ src/test/java/es/seresco/cursojee/FreddyEjercicioEspecie/service/impl/EspecieServiceTestCase.java
@@ -1,5 +1,39 @@
 package es.seresco.cursojee.FreddyEjercicioEspecie.service.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import es.seresco.cursojee.FreddyEjercicioEspecie.controller.dto.EspecieDto;
+import es.seresco.cursojee.FreddyEjercicioEspecie.core.Application;
+import es.seresco.cursojee.FreddyEjercicioEspecie.exceptions.MiValidationException;
+import es.seresco.cursojee.FreddyEjercicioEspecie.services.EspecieService;
+
+@RunWith(SpringRunner.class)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
+@TestPropertySource(locations = "classpath:application-test.yml")
 public class EspecieServiceTestCase {
 
+	@Autowired
+	private EspecieService especieService;
+	
+	@Test
+	@DisplayName("Obtener todas las familias de dinosaurios")
+	public void testFindAll() throws MiValidationException {
+		List<EspecieDto> resultado= especieService.findEspecies();
+		assertNotNull(resultado);
+		assertEquals("Se esperan 3 familias", 3, resultado.size());
+	}
 }
