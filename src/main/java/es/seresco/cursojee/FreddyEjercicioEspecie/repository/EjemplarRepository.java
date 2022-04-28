6f0fd5139d440e00ea9fc4c2ca29d5c52b9866a2
freddy <freddyfz2312@gmail.com>
2022-04-28 12:33:59 +0200

    Herb vs Carn funciona

--- src/main/java/es/seresco/cursojee/FreddyEjercicioEspecie/repository/EjemplarRepository.java
+++ src/main/java/es/seresco/cursojee/FreddyEjercicioEspecie/repository/EjemplarRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import es.seresco.cursojee.FreddyEjercicioEspecie.model.Ejemplar;
-import es.seresco.cursojee.FreddyEjercicioEspecie.model.EspecieTipoAlimentacion;
 import es.seresco.cursojee.FreddyEjercicioEspecie.model.TipoAlimentacion;
 
 @Repository
@@ -17,19 +16,10 @@ public interface EjemplarRepository extends JpaRepository<Ejemplar, Long>{
 	
 	@Query("SELECT e.recinto.tipoAlimentacion FROM Ejemplar e WHERE e.recinto.id = ?1")
 	public TipoAlimentacion getTipoAlimentacion(Long idRecinto);
-	
-	@Query("SELECT et FROM EspecieTipoAlimentacion et WHERE et.idEspecie = "
-			+ "(SELECT e.id FROM et.especie e WHERE e.id=?1) "
-			+ "AND "
-			+ "et.idTipoAlimentacion = "
-			+ "(SELECT ta.id FROM et.tipoAlimentacion ta WHERE ta.id = "
-			+ "(SELECT e.id FROM et.especie e WHERE e.id=?1))")
-	public EspecieTipoAlimentacion comprobarTipoAlimentacion(Long idEspecie,Long idRecinto);
 
-	@Query("SELECT ejemplar.especieTipoAlimentacion.tipoAlimentacion FROM Ejemplar ejemplar "
-			+ "WHERE ejemplar.especie.id = ?1")
+	@Query("SELECT et.tipoAlimentacion.id FROM EspecieTipoAlimentacion et WHERE et.idEspecie = (SELECT e.id FROM et.especie e WHERE e.id=?1)")
 	public Long comprobarTipoAlimentacionEspecie(Long idEspecie);
 
-	@Query("SELECT r.tipoAlimentacion FROM Recinto r WHERE r.id = ?1")
+	@Query("SELECT r.tipoAlimentacion.id FROM Recinto r WHERE r.id = ?1")
 	public Long comprobarTipoAlimentacionRecinto(Long idRecinto);
 }
